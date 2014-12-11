80973f719d1c4332a04d5b68b377f8734505f40a
binRick <rapidvps@gmail.com>
2014-12-11 01:30:17 -0500

    testing

--- index.js
+++ index.js
@@ -2,4 +2,9 @@
 
 var fs = require('fs');
 
+var daemontools = require('daemontools');
 
+daemontools.svstat('/service/nginx', function(err, stats) {
+    if (err) throw err;
+    console.log(stats);
+});
