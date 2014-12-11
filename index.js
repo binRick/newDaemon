5511b5e22d57c5ec58ec8c7373740922f92212af
binRick <rapidvps@gmail.com>
2014-12-11 02:35:15 -0500

    new

--- index.js
+++ index.js
@@ -6,27 +6,51 @@ var mkdirp = require('mkdirp'),
     service = process.argv[2] || 'none',
     dir = '/service/' + service,
     daemontools = require('daemontools');
+var prompt = require('prompt');
+
+
 daemontools.svstat(dir, function(err, stats) {
     if (err && err.code == 'ENOENT') {
-        console.log(cls.yellow('Attempting Service Install'));
-        mkdirp(dir + '/log/main', function(err) {
-            if (err) console.error(err);
-            var run = '#!/bin/sh' +
-                'export PORT=3000' +
-                'export USER=nobody' +
-                'cd ' + dir + '' +
-                'exec setugidlimit ${USER} node ./Server.js 2>&1';
-            var log = '#!/bin/sh' +
-                'export RUN_AS="nobody"' +
-                'mkdir ./main 2>/dev/null' +
-                'exec setuidgid ${RUN_AS} multilog t ./main';
-            fs.writeFileSync(dir + '/run', run, {
-                mode: '0755'
+        //        console.log(cls.yellow('Attempting Service Install to ' + dir + '. ok?'));
+        prompt.start();
+
+        prompt.get([{
+            description: 'New Service Name',
+            type: 'string',
+            default: service,
+        }, ], function(e, result) {
+            mkdirp(dir, function(err) {
+                if (err && (err.code == 'EACCES' || err.code == 'ENOENT')) {
+                    console.log(cls.red('Failure to create directory ' + dir));
+                    process.exit();
+                } else if (err) throw err;
+                setTimeout(function() {
+                    mkdirp(dir + '/log/main', function(err) {
+                        var run = '#!/bin/sh\n' +
+                            'export PORT="3000"\n' +
+                            'export USER="nobody"\n' +
+                            'export SCRIPT="./Server.js"\n' +
+                            'cd ' + dir + '\n' +
+                            'exec setuidgid ${USER} ${SCRIPT} 2>&1';
+                        var log = '#!/bin/sh\n' +
+                            'export RUN_AS="root"\n' +
+                            'mkdir ./main 2>/dev/null\n' +
+                            'exec setuidgid ${RUN_AS} multilog t ./main\n';
+                        var bin = '#!/usr/bin/env node\n' +
+                            'var fs = require(\'fs\');\nconsole.log(\'Testing...\');';
+                        fs.writeFileSync(dir + '/run', run, {
+                            mode: '0755'
+                        });
+                        fs.writeFileSync(dir + '/Server.js', bin, {
+                            mode: '0755'
+                        });
+                        fs.writeFileSync(dir + '/log/run', log, {
+                            mode: '0755'
+                        });
+                        console.log(cls.green('Installed to ' + dir + '\n\n'));
+                    });
+                }, 250);
             });
-            fs.writeFileSync(dir + '/log/run', run, {
-                mode: '0755'
-            });
-            console.log(cls.green('Installed to ' + dir + '\n\n'));
         });
     } else {
         console.log(stats);
