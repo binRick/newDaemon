6527a1f6988f53ac5faff50df0836d14c4634372
binRick <rapidvps@gmail.com>
2014-12-11 19:48:19 -0500

    updates

--- index.js
+++ index.js
@@ -13,26 +13,34 @@ daemontools.svstat(dir, function(err, stats) {
     if (err && err.code == 'ENOENT') {
         prompt.start();
         prompt.get([{
+                name: 'service',
                 description: 'New Service Name',
                 type: 'string',
                 default: service,
-            },
-
-            {
+            }, {
+                name: 'port',
                 description: 'New Service Port',
                 type: 'number',
                 default: '3000',
             }, {
+                name: 'script',
                 description: 'New Service Script',
                 type: 'string',
                 default: 'Server.js',
             }, {
+                name: 'user',
                 description: 'New Service User',
                 type: 'string',
                 default: 'nobody',
+            }, {
+                name: 'bin',
+                description: 'New Service Interpretter',
+                type: 'string',
+                default: 'node',
             },
 
         ], function(e, result) {
+            dir = '/service/' + result.service;
             mkdirp(dir, function(err) {
                 if (err && (err.code == 'EACCES' || err.code == 'ENOENT')) {
                     console.log(cls.red('Failure to create directory ' + dir));
@@ -41,21 +49,22 @@ daemontools.svstat(dir, function(err, stats) {
                 setTimeout(function() {
                     mkdirp(dir + '/log/main', function(err) {
                         var run = '#!/bin/sh\n' +
-                            'export PORT="3000"\n' +
-                            'export USER="nobody"\n' +
-                            'export SCRIPT="./Server.js"\n' +
+                            'export PORT="' + result.port + '"\n' +
+                            'export USER="' + result.user + '"\n' +
+                            'export BIN="' + result.bin + '"\n' +
+                            'export SCRIPT="' + result.script + '"\n' +
                             'cd ' + dir + '\n' +
-                            'exec setuidgid ${USER} ${SCRIPT} 2>&1';
+                            'exec setuidgid ${USER} ${BIN} ./${SCRIPT} 2>&1';
                         var log = '#!/bin/sh\n' +
                             'export RUN_AS="root"\n' +
                             'mkdir ./main 2>/dev/null\n' +
                             'exec setuidgid ${RUN_AS} multilog t ./main\n';
                         var bin = '#!/usr/bin/env node\n' +
-                            'var fs = require(\'fs\');\nconsole.log(\'Testing...\');';
+                            'var fs = require(\'fs\');\nconsole.log(\'Testing...\');\nsetTimeout(function(){}, 5000);\n';
                         fs.writeFileSync(dir + '/run', run, {
                             mode: '0755'
                         });
-                        fs.writeFileSync(dir + '/Server.js', bin, {
+                        fs.writeFileSync(dir + '/' + result.script, bin, {
                             mode: '0755'
                         });
                         fs.writeFileSync(dir + '/log/run', log, {
